09da37af5e4d74772a86abf1e927cc99789dc2fa
imtiyazhasan1 <98658942+imtiyazhasan1@users.noreply.github.com>
2024-02-17 13:32:36 +0530

    Create variable.tf

---
+++ akswithvariable/variable.tf
@@ -0,0 +1,28 @@
+variable "subnet_ids" {
+  type    = list(string)
+  default = ["subnet_id_1", "subnet_id_2"]
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "aks_qatar_ipay_dev_vnet"
+}
+
+variable "app_pool_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "app_pool_max_size" {
+  type    = number
+  default = 10
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {
+    Environment         = "Production"
+    "support team"      = "Qatar Cloud Team"
+    "maintenance window" = "10PM to 00:30 AM on Monday"
+  }
+}
